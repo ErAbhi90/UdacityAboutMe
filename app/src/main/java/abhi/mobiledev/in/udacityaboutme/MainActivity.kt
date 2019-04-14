3f4d38de77f73cb1a4cccd770a3027b4e130c37b
ErAbhi90 <er.abhithakral@gmail.com>
2019-04-15 01:47:12 +0530

    Added Code to add Nickname and show it in text view

--- app/src/main/java/abhi/mobiledev/in/udacityaboutme/MainActivity.kt
+++ app/src/main/java/abhi/mobiledev/in/udacityaboutme/MainActivity.kt
@@ -1,12 +1,34 @@
 package abhi.mobiledev.`in`.udacityaboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_done.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+
+        nickname_text.text = nickname_edit.text
+        nickname_edit.visibility = View.GONE
+        view.visibility = View.GONE
+
+        nickname_text.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
